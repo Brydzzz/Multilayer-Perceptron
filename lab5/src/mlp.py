7e67c8b4a2ad823cc812091aee73bc57a6449f95
bsilawko <mrufkazkrem@protonmail.com>
2025-01-01 17:59:40 +0100

    deep focus session 2

--- lab5/src/mlp.py
+++ lab5/src/mlp.py
@@ -1,49 +1,67 @@
 import numpy as np
-from typing import Callable
+from typing import Callable, Tuple
 
 
-class Neuron:
-    def __init__(self, nin):
-        self.weights = [np.random.uniform(-1, 1) for _ in range(nin)]
-        self.biases = [np.random.uniform(-1, 1) for _ in range(nin)]
 
-
+# TODO: get rid of Layer class
 class Layer:
-    def __init__(self, nin, nout):
-        self.neurons = [Neuron(nin) for _ in range(nout)]
+    def __init__(self, nout, nin, biases):
+        # self.weights = [
+        #     [np.random.uniform(-1, 1) for _ in range(nin)] for _ in range(nout)
+        # ]
+        # self.biases = [
+        #     [np.random.uniform(-1, 1) for _ in range(nin)] for _ in range(nout)
+        # ]
+        """
+        self.weights -> input weights for each neuron in the layer
+        self.biases -> input biases for each neuron in the layer
+        """
+        self.weights = np.random.uniform(-1, 1, size=(nout, nin))
+        self.biases = (
+            biases if biases else np.random.uniform(-1, 1, size=(nout))
+        )
+
+    def calculate_z(self, activations):
+        return np.dot(self.weights, activations) + self.biases
 
 
 class MLP:
     def __init__(
         self,
-        layers_num: int,
-        neurons_num: np.ndarray,
+        layers_sizes: list[int],
         loss_func: Callable,
-        activation_func: Callable,
+        activation_func: Callable[[np.ndarray], np.ndarray],
+        loss_derv: Callable[[np.n]],
     ):
-        if len(neurons_num) != layers_num:
-            raise ValueError(
-                "Neuron numbers length do not match layers number"
-            )
-        self.layers = self.__init_layers(layers_num, neurons_num)
+        self.layers = self.__init_layers(layers_sizes)
         self.loss = loss_func
         self.activation = activation_func
+        self.loss_derv = loss_derv
 
-    def __init_layers(self, lnum, nnum):
+    def __init_layers(self, lsizes):
         layers = []
-        for i in range(lnum):
+        for i in range(lsizes):
             if i == 0:
-                layers.append(Layer(0, nnum[i + 1]))
-            elif i == lnum - 1:
-                layers.append(Layer(nnum[i], 0))
+                layers.append(Layer(0, 0, biases=np.zeros(lsizes[i])))
+
             else:
-                layers.append(Layer(nnum[i], nnum[i + 1]))  # Hidden Layers
+                layers.append(Layer(lsizes[i], lsizes[i - 1]))  # Hidden Layers
         return layers
 
-    def forward(self):
-        pass
+    def feed_forward(
+        self, a: np.ndarray
+    ) -> Tuple[list[np.ndarray], list[np.ndarray]]:
+        activs = [a]
+        zs = []
+        for layer in self.layers:
+            z = layer.calculate_z(a)
+            zs.append(z)
+            a = self.activation(z)
+            activs.append(a)
 
-    def backward(self):
+        return activs, zs
+
+    def backward(self, targets: np.ndarray):
         pass
 
     def train(self, epoch):
