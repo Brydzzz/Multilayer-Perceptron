3fe6fcecc61f465d99d1c7ce39f882ccf4c6c13c
bsilawko <mrufkazkrem@protonmail.com>
2025-01-02 16:11:29 +0100

    got rid of Layer class and other small changes

--- lab5/src/mlp.py
+++ lab5/src/mlp.py
@@ -2,70 +2,72 @@ import numpy as np
 from typing import Callable, Tuple
 
 
-
-# TODO: get rid of Layer class
-class Layer:
-    def __init__(self, nout, nin, biases):
-        # self.weights = [
-        #     [np.random.uniform(-1, 1) for _ in range(nin)] for _ in range(nout)
-        # ]
-        # self.biases = [
-        #     [np.random.uniform(-1, 1) for _ in range(nin)] for _ in range(nout)
-        # ]
-        """
-        self.weights -> input weights for each neuron in the layer
-        self.biases -> input biases for each neuron in the layer
-        """
-        self.weights = np.random.uniform(-1, 1, size=(nout, nin))
-        self.biases = (
-            biases if biases else np.random.uniform(-1, 1, size=(nout))
-        )
-
-    def calculate_z(self, activations):
-        return np.dot(self.weights, activations) + self.biases
-
-
 class MLP:
     def __init__(
         self,
         layers_sizes: list[int],
         loss_func: Callable,
         activation_func: Callable[[np.ndarray], np.ndarray],
-        loss_derv: Callable[[np.n]],
+        loss_derv: Callable,
     ):
-        self.layers = self.__init_layers(layers_sizes)
+        self.weights = self.__init_weights(layers_sizes)
+        self.biases = self.__init_biases(layers_sizes)
         self.loss = loss_func
         self.activation = activation_func
         self.loss_derv = loss_derv
 
-    def __init_layers(self, lsizes):
-        layers = []
-        for i in range(lsizes):
-            if i == 0:
-                layers.append(Layer(0, 0, biases=np.zeros(lsizes[i])))
+    def __init_weights(self, lsizes):
+        return [
+            np.random.uniform(low=-1, high=1, size=(nout, nin))
+            for nout, nin in zip(lsizes[1:], lsizes[:-1])
+        ]
 
-            else:
-                layers.append(Layer(lsizes[i], lsizes[i - 1]))  # Hidden Layers
-        return layers
+    def __init_biases(self, lsizes):
+        return [
+            np.random.uniform(low=-1, high=1, size=(n)) for n in lsizes[1:]
+        ]
 
-    def feed_forward(
+    def feed_forward(self, a: np.ndarray) -> np.ndarray:
+        for w, b in zip(self.weights, self.biases):
+            z = np.dot(w, a) + b
+            a = self.activation(z)
+        return a
+
+    def get_activations_and_zs(
         self, a: np.ndarray
     ) -> Tuple[list[np.ndarray], list[np.ndarray]]:
         activs = [a]
         zs = []
-        for layer in self.layers:
-            z = layer.calculate_z(a)
+        for w, b in zip(self.weights, self.biases):
+            z = np.dot(w, a) + b
             zs.append(z)
             a = self.activation(z)
             activs.append(a)
-
         return activs, zs
 
-    def backward(self, targets: np.ndarray):
+    def backward(
+        self, x: np.ndarray, targets: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray]:
         pass
 
-    def train(self, epoch):
+    def train(
+        self,
+        training_data: np.ndarray,
+        epochs: int,
+        mini_batch_size: int,
+        learning_rate: float,
+    ) -> None:
         pass
 
-    def predict(self):
+    def predict(self, X: np.ndarray) -> list[int]:
+        """
+        A method that returns predicted number
+        for each sample in MNIST dataset.
+
+        Args:
+            X (np.ndarray): The MNIST dataset.
+
+        Returns:
+            list[int]: Predicted number for each sample in X.
+        """
         pass
