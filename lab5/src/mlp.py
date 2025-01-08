ed7ba9672b5828c349f974b3cceded48fb125a2f
Elbergg <tom.p.smolen@gmail.com>
2025-01-08 16:18:04 +0100

    docstrings and typehints

--- lab5/src/mlp.py
+++ lab5/src/mlp.py
@@ -4,18 +4,19 @@ from typing import Callable, Tuple
 
 
 class MLP:
+
     def __init__(
         self,
         layers_sizes: list[int],
-        loss_func: Callable,
+        loss_func: Callable[[np.ndarray, np.ndarray], np.ndarray],
         activation_func: Callable[[np.ndarray], np.ndarray],
-        loss_derv: Callable,
-        activation_derv: Callable,
-        output_func: Callable,
-        output_derv: Callable,
-        target_fit: Callable,
-        weight_init: Callable,
-        bias_init: Callable,
+        loss_derv: Callable[[np.ndarray, np.ndarray], np.ndarray],
+        activation_derv: Callable[[np.ndarray], np.ndarray],
+        output_func: Callable[[np.ndarray], np.ndarray],
+        output_derv: Callable[[np.ndarray], np.ndarray],
+        target_fit: Callable[[np.ndarray], np.ndarray],
+        weight_init: Callable[[int, int], np.ndarray],
+        bias_init: Callable[[int], np.ndarray],
     ):
         self.loss = loss_func
         self.activation = activation_func
@@ -29,16 +30,19 @@ class MLP:
         self.weights = self.__init_weights(layers_sizes)
         self.biases = self.__init_biases(layers_sizes)
 
-    def __init_weights(self, lsizes):
+    def __init_weights(self, lsizes: list[int]):
         return [
             self.weight_init(nout, nin)
             for nout, nin in zip(lsizes[1:], lsizes[:-1])
         ]
 
-    def __init_biases(self, lsizes):
+    def __init_biases(self, lsizes: list[int]):
         return [self.bias_init(n) for n in lsizes[1:]]
 
     def feed_forward(self, a: np.ndarray) -> np.ndarray:
+        """
+        Goes forward in the neural network, returns output layer activations
+        """
         a = a.reshape(a.shape[0], 1)
         for w, b in zip(self.weights[:-1], self.biases[:-1]):
             z = np.dot(w, a) + b
@@ -50,6 +54,10 @@ class MLP:
     def get_activations_and_zs(
         self, a: np.ndarray
     ) -> Tuple[list[np.ndarray], list[np.ndarray]]:
+        """
+        Return lists of activation values and z vectors
+        for the entire network, used for backpropagation
+        """
         a = a.reshape(a.shape[0], 1)
         activs = [a]
         zs = []
@@ -67,11 +75,13 @@ class MLP:
     def backward(
         self, a: np.ndarray, targets: np.ndarray
     ) -> Tuple[np.ndarray, np.ndarray]:
-
+        """
+        Goes backward in the neural network, returns partial derivatives
+        for all weights and biases
+        """
         weight_dervs = [np.zeros(w.shape) for w in self.weights]
         bias_dervs = [np.zeros(b.shape) for b in self.biases]
         activs, zs = self.get_activations_and_zs(a)
-        # targets = targets.reshape(targets.shape[0], 1)
         loss_derv = self.loss_derv(activs[-1], targets) * self.output_derv(
             zs[-1]
         )
@@ -91,8 +101,21 @@ class MLP:
         epochs: int,
         mini_batch_size: int,
         learning_rate: float,
-        class_column: str,
+        class_column: str = "target",
     ) -> None:
+        """
+        Trains the network on given training_data.
+
+        Args:
+            training_data (pd.DataFrame): The dataset.
+            epochs (int): How many epochs.
+            mini_batch_size (int) Batch size.
+            learning rate (float) Used for gradient descent.
+            class_column (float) used to seperate the classes from the dataset
+
+        Returns:
+            Nothing
+        """
         for epoch in range(epochs):
             mini_batches = self.initialize_mini_batches(
                 training_data, mini_batch_size
@@ -102,6 +125,10 @@ class MLP:
             print(f"Epoch {epoch + 1} completed.")
 
     def process_batch(self, batch, class_column, learning_rate):
+        """
+        Use backward to get gradients for each test case
+        then calculate the average gradients and do gradient descent
+        """
         weight_gradient = [np.zeros(w.shape) for w in self.weights]
         bias_gradient = [np.zeros(b.shape) for b in self.biases]
         classes = batch[class_column].to_numpy()
@@ -127,6 +154,9 @@ class MLP:
     def initialize_mini_batches(
         self, t_data: pd.DataFrame, mini_batch_size: int
     ):
+        """
+        Split the training data into mini batches of a given size
+        """
         mini_batches = []
         while not t_data.empty:
             mini_batch = t_data.sample(n=min(mini_batch_size, len(t_data)))
@@ -136,14 +166,13 @@ class MLP:
 
     def predict(self, X: pd.DataFrame) -> np.ndarray[float]:
         """
-        A method that returns predicted class
-        from the dataset given.
+        Returns the activations of the output layer
 
         Args:
             X (pd.DataFrame): The dataset.
 
         Returns:
-            list[int]: Predicted class
+            list[int]: Network output
         """
         predictions = []
         for _, row in X.iterrows():
